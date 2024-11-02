61027d062517ca6133aaa04e524cda8d29d49196
CapiJhosep <jhoseplamec1417@gmail.com>
2024-11-02 10:25:47 -0500

    commit

--- src/condicionales/frm32.java
+++ src/condicionales/frm32.java
@@ -11,16 +11,16 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
-public class frm31 extends JFrame {
+public class frm32 extends JFrame {
 	private static final long serialVersionUID = 1L;
-	JTextField txtHoras, txtCategoria;
+	JTextField txtCategoria, txtPromedio;
 	JTextArea txaRpta;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					frm31 frame = new frm31();
+					frm32 frame = new frm32();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -29,31 +29,31 @@ public class frm31 extends JFrame {
 		});
 	}
 
-	public frm31() {
+	public frm32() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 400, 300);
 		getContentPane().setLayout(null);
 		setLocationRelativeTo(null);
 		
-		JLabel lblHoras = new JLabel("Horas :");
-		lblHoras.setBounds(50, 20, 80, 30);
-		getContentPane().add(lblHoras); 
-		
 		JLabel lblCategoria = new JLabel("Categoría :");
-		lblCategoria.setBounds(180, 20, 80, 30);
+		lblCategoria.setBounds(40, 20, 80, 30);
 		getContentPane().add(lblCategoria); 
 		
-		txtHoras = new JTextField();
-		txtHoras.setBounds(100, 20, 70, 30);
-		txtHoras.setHorizontalAlignment( SwingConstants.RIGHT);
-		txtHoras.setMargin( new Insets(5, 5, 5, 5));
-		getContentPane().add(txtHoras);
+		JLabel lblPromedio = new JLabel("Promedio :");
+		lblPromedio.setBounds(190, 20, 80, 30);
+		getContentPane().add(lblPromedio); 
 		
 		txtCategoria = new JTextField();
-		txtCategoria.setBounds(250, 20, 70, 30);
+		txtCategoria.setBounds(110, 20, 70, 30);
 		txtCategoria.setHorizontalAlignment( SwingConstants.RIGHT);
 		txtCategoria.setMargin( new Insets(5, 5, 5, 5));
 		getContentPane().add(txtCategoria);
+		
+		txtPromedio = new JTextField();
+		txtPromedio.setBounds(260, 20, 70, 30);
+		txtPromedio.setHorizontalAlignment( SwingConstants.RIGHT);
+		txtPromedio.setMargin( new Insets(5, 5, 5, 5));
+		getContentPane().add(txtPromedio);
 		
 		txaRpta = new JTextArea();
 		txaRpta.setFocusable(false);
@@ -73,42 +73,28 @@ public class frm31 extends JFrame {
 	}
 
 	protected void btnCalcular_actionPerformed() {
-	    double horas = Double.parseDouble(txtHoras.getText()); 
 	    String categoria = txtCategoria.getText();
 	    String cat = categoria.toUpperCase();
-	    double pago = 0;
-	    double descuento;
-	    double sueldo = 0;
+	    double promedio = Integer.parseInt(txtPromedio.getText());
+	    double descuento = 0;
+	    double pension = 0;
 	    
-	    if (cat.equals("A")) {
-	        pago = 21; 
-	        sueldo = horas * pago;
-	    }
-	    else if (cat.equals("B")) {
-	        pago = 19.50; 
-	        sueldo = horas * pago;
-	    }
-	    else if (cat.equals("C")) {
-	        pago = 17; 
-	        sueldo = horas * pago;
-	    }
-	    else if (cat.equals("D")) {
-	        pago = 15.50; 
-	        sueldo = horas * pago;
-	    }
+	    if (cat.equals("A")) pension = 550;
+	    else if (cat.equals("B")) pension = 500;
+	    else if (cat.equals("C")) pension = 450;
+	    else if (cat.equals("D")) pension = 400;
 	    
-	    if (sueldo > 2500) {
-	        descuento = sueldo * 0.2;
-	    } 
-	    else {
-	        descuento = sueldo * 0.15;
-	    }
+	    if (promedio < 14) descuento = 0;
+	    else if (promedio >= 14 && promedio < 16) descuento = pension * 0.1;
+	    else if (promedio >= 16 && promedio < 18) descuento = pension * 0.12;
+	    else if (promedio >= 18 && promedio <= 20) descuento = pension * 0.15;
 	    
-	    double neto = sueldo - descuento;
+	    double pago = pension - descuento;
 	    
 	    txaRpta.setText("");
-	    txaRpta.append(String.format("Sueldo Bruto: \tS/%.2f\n", sueldo));
+	    txaRpta.append(String.format("Pensión: \tS/%.2f\n", pension));
 	    txaRpta.append(String.format("Descuento: \tS/%.2f\n", descuento));
-	    txaRpta.append(String.format("Sueldo Neto: \tS/%.2f\n", neto));
+	    txaRpta.append(String.format("Pago Total: \tS/%.2f\n", pago));
+	    
 	}
 }
